bfb6107a091aa21e1fcab5bf67269e1bc98311b8
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-10-06 04:41:15 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Mój Maczek on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
